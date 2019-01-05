75bb102fbb0d918c53387dc2b5e57125560b7e1c
Liam <lk@alienbuild.uk>
2019-01-05 22:36:52 +0000

    Added endpoint filters

--- symbols/symbol.js
+++ symbols/symbol.js
@@ -2,33 +2,33 @@ const fetch = require("node-fetch");
 
 var symbol = (symbol, callback) => {
 
-    const symbols = symbol;
-
-    fetch(` https://api.iextrading.com/1.0/stock/market/batch?symbols=${symbols}&types=quote,chart&range=1d&chartInterval=30`)
+    fetch(`https://api.iextrading.com/1.0/stock/market/batch?symbols=${symbol}&types=quote,chart&range=1d&chartInterval=30&filter=symbol,companyName,primaryExchange,change,volume,minute,close,label,date`)
         .then(res=>res.json())
         .then(data=>{
 
             callback(undefined, {
-                company: data[symbols.toUpperCase()].quote.companyName,
-                symbol: data[symbols.toUpperCase()].quote.symbol,
-                exchange: data[symbols.toUpperCase()].quote.primaryExchange,
-                date: data[symbols.toUpperCase()].chart[0].date,
-                open: data[symbols.toUpperCase()].quote.open,
-                firstInt: [{
-                    volume: data[symbols.toUpperCase()].chart[0].volume,
-                    close: data[symbols.toUpperCase()].chart[0].close,
-                    time: data[symbols.toUpperCase()].chart[0].label
-                }],
-                secondInt: [{
-                    volume: data[symbols.toUpperCase()].chart[1].volume,
-                    close: data[symbols.toUpperCase()].chart[1].close,
-                    time: data[symbols.toUpperCase()].chart[1].label
-                }],
-                thirdInt: [{
-                    volume: data[symbols.toUpperCase()].chart[2].volume,
-                    close: data[symbols.toUpperCase()].chart[2].close,
-                    time: data[symbols.toUpperCase()].chart[2].label
-                }]
+                company: data[symbol.toUpperCase()].quote.companyName,
+                symbol: data[symbol.toUpperCase()].quote.symbol,
+                exchange: data[symbol.toUpperCase()].quote.primaryExchange,
+                date: data[symbol.toUpperCase()].chart[0].date,
+                open: data[symbol.toUpperCase()].quote.open,
+                chart: [
+                    {
+                        volume: data[symbol.toUpperCase()].chart[0].volume,
+                        close: data[symbol.toUpperCase()].chart[0].close,
+                        time: data[symbol.toUpperCase()].chart[0].label
+                    },
+                    {
+                        volume: data[symbol.toUpperCase()].chart[1].volume,
+                        close: data[symbol.toUpperCase()].chart[1].close,
+                        time: data[symbol.toUpperCase()].chart[1].label
+                    },
+                    {
+                        volume: data[symbol.toUpperCase()].chart[2].volume,
+                        close: data[symbol.toUpperCase()].chart[2].close,
+                        time: data[symbol.toUpperCase()].chart[2].label
+                    }
+                ]
             });
 
         });
